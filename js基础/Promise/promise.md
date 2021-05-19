1109eb2f1f62b7d6ebfda6b226a70be0e635e5a7
linan31 <linan31@58.com>
2021-05-19 22:58:40 +0800

    feat: 垃圾回收.xmind & 深浅拷贝补充

---
+++ js基础/Promise/promise.md
@@ -0,0 +1,4 @@
+## 参考文章
+1. 手写一个Promise/A+,完美通过官方872个测试用例：https://segmentfault.com/a/1190000023157856
+
+2. Promise的源码实现(完美符合Promise/A+规范):https://juejin.cn/post/6844903796129136654
--- js基础/深浅拷贝/clone1.js
+++ js基础/深浅拷贝/clone1.js
@@ -1,7 +1,6 @@
-function getType(target) {
-    return Object.prototype.toString.call(target)
-}
-
+// 最基础版deepClone，用递归解决多层深度的问题
+// 只考虑了最普通的Object和Array
+// 有循环引用的问题，递归会进入死循环导致栈溢出
 function deepClone(target) {
     if(target instanceof Object){
         let cloneTarget = Array.isArray(target) ? [] : {}
@@ -21,14 +20,25 @@ var obj1 = {
         age:'3',
         say:() => {console.log('爱小楠')}
     },
+    arr:['1','2'],
     x:undefined,
     y:null,
     z:Symbol()
 }
 
 var obj2 = deepClone(obj1)
+// obj2 {
+//       name: '小楠',
+//       child: { name: '华华', age: '3', say: {} },
+//       arr: [ '1', '2' ],
+//       x: undefined,
+//       y: null,
+//       z: Symbol()
+//      }
 
 
+// 解决循环引用版deepClone
+// 用一个map来存储当前对象和拷贝对象的对应关系
 function deepClone1(target,map = new Map()) {
     if(target instanceof Object){
         let cloneTarget = Array.isArray(target) ? [] : {}
@@ -60,4 +70,4 @@ target.foo = target
 
 var copyTarget = deepClone1(target)
 
-console.log(copyTarget)
+// console.log(copyTarget)
